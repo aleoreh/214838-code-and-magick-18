d88f4dd2e7bf066ed69f46a38e4e774b4f45d9df
aleoreh <aleoreh@gmail.com>
2019-09-16 19:26:32 +0300

    Fix reviewer issues

--- js/stat.js
+++ js/stat.js
@@ -6,6 +6,8 @@ var STAT_HEIGHT = 270;
 var STAT_WIDTH = 420;
 var STAT_OFFSET = 10;
 
+var STAT_TEXT_HEIGHT = 25;
+
 var BAR_HEIGHT = 150;
 var BAR_COLUMN_WIDTH = 40;
 var BAR_COLUMN_PADDING = 50;
@@ -16,47 +18,45 @@ var drawText = function (ctx, x, y) {
   ctx.textBaseline = 'top';
   ctx.font = '16px "PT Mono"';
   ctx.fillText('Ура вы победили!', x, y);
-  ctx.fillText('Список результатов:', x, y + 25);
+  ctx.fillText('Список результатов:', x, y + STAT_TEXT_HEIGHT);
+};
+
+var drawItem = function (ctx, itemIndex, x, y, names, times) {
+  var maxTime = Math.max.apply(null, times);
+  var itemHeight = times[itemIndex] * (BAR_HEIGHT - BAR_TEXT_HEIGHT) / maxTime;
+
+  var itemX = x + (itemIndex + 1) * BAR_COLUMN_PADDING + itemIndex * BAR_COLUMN_WIDTH;
+  var itemY = y + BAR_HEIGHT - itemHeight;
+
+  ctx.fillStyle = names[itemIndex] === 'Вы'
+    ? 'rgba(255, 0, 0, 1)'
+    : 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+
+  ctx.fillText(
+      Math.round(times[itemIndex]),
+      itemX,
+      y + BAR_HEIGHT - itemHeight - BAR_TEXT_HEIGHT
+  );
+
+  ctx.fillRect(
+      itemX,
+      itemY,
+      BAR_COLUMN_WIDTH,
+      itemHeight
+  );
+
+  ctx.fillText(
+      names[itemIndex],
+      itemX,
+      y + BAR_HEIGHT + BAR_TEXT_HEIGHT
+  );
 };
 
 var drawStatistics = function (ctx, x, y, names, times) {
-
-  var drawItem = function (i) {
-    var itemHeight = times[i] * (BAR_HEIGHT - BAR_TEXT_HEIGHT) / maxTime;
-
-    var itemX = x + (i + 1) * BAR_COLUMN_PADDING + i * BAR_COLUMN_WIDTH;
-    var itemY = y + BAR_HEIGHT - itemHeight;
-
-    ctx.fillStyle = names[i] === 'Вы'
-      ? 'rgba(255, 0, 0, 1)'
-      : 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
-
-    ctx.fillText(
-        Math.round(times[i]),
-        itemX,
-        y + BAR_HEIGHT - itemHeight - BAR_TEXT_HEIGHT
-    );
-
-    ctx.fillRect(
-        itemX,
-        itemY,
-        BAR_COLUMN_WIDTH,
-        itemHeight
-    );
-
-    ctx.fillText(
-        names[i],
-        itemX,
-        y + BAR_HEIGHT + BAR_TEXT_HEIGHT
-    );
-  };
-
   var namesCount = Math.min(names.length, times.length);
-  var maxTime = Math.max.apply(null, times);
-
 
   for (var i = 0; i < namesCount; i++) {
-    drawItem(i);
+    drawItem(ctx, i, x, y, names, times);
   }
 };
 
